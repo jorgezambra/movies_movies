adc4a55b3ff681b6ac1e2f68f3af64d7d43c393b
jorgezambra <jorgezambra@gmail.com>
2020-10-19 09:51:52 -0400

    .cache

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -30323,7 +30323,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58056" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58164" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
